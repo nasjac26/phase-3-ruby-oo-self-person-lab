b57495db8fa19dacbe3cebbc67b3d0109d66b8c4
Jacob <Jacobonasrallah@gmail.com>
2021-10-17 08:51:37 -0700

    rspec broken

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,41 @@
 # your code goes here
+class Person
+    attr_reader :name
+    attr_accessor :bank_account
+
+
+    def initialize(provided_name)
+        @name = provided_name
+        self.bank_account = 25
+        @happiness = 8
+        @hygiene = 8
+    end
+
+    def happiness
+        @happiness
+    end
+
+    def happiness=(provided_value)
+        if provided_value <= 10 && provided_value >= 0
+            @happiness = provided_value
+        elsif provided_value > 10
+            @happiness = 10
+        else
+            @happiness = 0
+        end
+
+    def hygiene
+        @hygiene
+    end
+
+    def hygiene=(provided_value)
+        if provided_value <= 10 && provided_value >= 0
+            @hygiene = provided_value
+        elsif provided_value > 10
+            @hygiene = 10
+        else
+            @hygiene = 0
+        end
+    end
+
+end
