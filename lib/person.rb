de8fd6d0ffcfdb327695e2b7dc56e5dbb5e5df5e
Jacob <Jacobonasrallah@gmail.com>
2021-10-17 10:34:40 -0700

    Completed assignment

--- lib/person.rb
+++ lib/person.rb
@@ -23,6 +23,7 @@ class Person
         else
             @happiness = 0
         end
+    end
 
     def hygiene
         @hygiene
@@ -36,6 +37,66 @@ class Person
         else
             @hygiene = 0
         end
+    end
+
+    def happy?
+        @happiness > 7 ? true : false
+    end
+
+    def clean? 
+        @hygiene > 7 ? true : false
+    end
+
+    def get_paid(provided_amount)
+        self.bank_account += provided_amount
+        "all about the benjamins"
+    end
+
+    def take_bath
+        current_hygiene_points = @hygiene += 4
+        self.hygiene=(current_hygiene_points)
+        "♪ Rub-a-dub just relaxing in the tub ♫"
+    end
+
+    def work_out
+        current_hygiene_points = @hygiene -= 3
+        self.hygiene=(current_hygiene_points)
+        current_happiness_points = @happiness += 2
+        self.happiness=(current_happiness_points)
+        "♪ another one bites the dust ♫"
+    end
+
+    def call_friend(another_instance_of_person)
+        this_instances_happiness = self.happiness + 3
+        a_friend_instances_happiness = another_instance_of_person.happiness + 3
+
+        self.happiness=(this_instances_happiness)
+        another_instance_of_person.happiness=(a_friend_instances_happiness)
+
+        "Hi #{another_instance_of_person.name}! It's #{self.name}. How are you?"
+    end
+
+    def start_conversation(friend_instance, given_topic)
+        case given_topic
+        
+        when "politics"
+            current_instance_happiness = self.happiness - 2
+            friend_instance_happiness = friend_instance.happiness - 2
+            self.happiness=(current_instance_happiness)
+            friend_instance.happiness=(friend_instance_happiness)
+            "blah blah partisan blah lobbyist"
+
+        when "weather"
+            current_instance_happiness = self.happiness + 1
+            friend_instance_happiness = friend_instance.happiness + 1
+            self.happiness=(current_instance_happiness)
+            friend_instance.happiness=(friend_instance_happiness)
+            "blah blah sun blah rain"
+
+        else
+            "blah blah blah blah blah"
+        end
+
     end
 
 end
